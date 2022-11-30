74e842b4b4ab7d875d73a601735f78d5ac3c30b1
NMcSheehy <nmflash8@byui.edu>
2022-11-30 15:34:14 -0700

    commit

---
+++ main.py
@@ -0,0 +1,26 @@
+def main():
+    pass
+
+class Director():
+    pass
+
+class Window():
+    pass
+
+class NPC():
+    pass
+
+class Zombies():
+    def __init__(self) -> None:
+        pass
+
+    def move(): 
+        pass
+
+
+class Villager():
+    pass
+
+
+if __name__ == '__main__':
+    main()
