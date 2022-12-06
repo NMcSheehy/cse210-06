bd02f3bc575763a9bbd7d0b82467723f700ebd82
PaulMcB1234 <113477302+PaulMcB1234@users.noreply.github.com>
2022-12-06 11:23:38 -0700

    commit

--- main.py
+++ main.py
@@ -1,25 +1,73 @@
+import pygame
+pygame.init()
+import random
+
 def main():
-    pass
+    window = Window()
+    
+    game = Director(60)
+    
+    game.start(window)
 
 class Director():
-    pass
+    def __init__(self, FPS):
+        self.playing = True
+        
+        self.clock = pygame.time.Clock()
+    
+    def start(self, window):
+        while self.playing:
+            self.clock.tick(self.FPS)
+            level = 1
+            match level:
+                case 1:
+                    pass
+                case 2:
+                    pass
+                case 3:
+                    pass
 
 class Window():
     pass
 
 class NPC():
-    pass
-
-class Zombies():
-    def __init__(self) -> None:
+    def __init__(self, path, width, height, x, y, speed, infection_rate):
+        self.path = path
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        self.speed = speed
+        self.infection_rate = infection_rate
+    
+    def move(self):
+        pass
+    
+    def infection(self):
         pass
 
-    def move(): 
+class Zombie(NPC):
+    def __init__(self):
         pass
 
+    def move(self): 
+        pass
 
-class Villager():
-    pass
+    def infection(self):
+        pass    
+
+class Villager(NPC):
+    def __init__(self):
+        pass
+
+    def move(self): 
+        pass
+
+    def infection(self):
+        pass
+    
+    def change(self):
+        pass
 
 
 if __name__ == '__main__':
